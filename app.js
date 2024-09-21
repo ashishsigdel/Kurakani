660dc8054b1357002dc332c8da73b4cf4d9f7698
ashish <a.asis.sigdel01@gmail.com>
2024-09-21 16:24:20 +0545

    Test Route, Not Found, ApiError

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 import express from "express";
 import { createServer } from "http";
+import cors from "cors";
+import ApiError from "./utils/apiError.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 const httpServer = createServer(app);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Api Working..." });
+});
+
+// if route is api route, send 404 in json else render 404 page
+app.use((req, res, next) => {
+  throw new ApiError({
+    status: 404,
+    message: "URL Not Found",
+    errors: [
+      {
+        message: `Cannot ${req.method} ${req.originalUrl}`,
+      },
+    ],
+  });
+});
 
 export { app };
 
