951c97b69f0caafa6e18881bfb77f8da8f728468
ashish <a.asis.sigdel01@gmail.com>
2024-09-21 18:50:01 +0545

    refreshToken model

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,6 @@
 import { Sequelize, DataTypes, Op, sequelize } from "../database/dbConfig.js";
 import User from "./user.model.js";
+import RefreshToken from "./refreshtoken.model.js";
 
 const db = {};
 
@@ -9,5 +10,6 @@ db.DataTypes = DataTypes;
 db.Op = Op;
 
 db.User = User(sequelize, Sequelize, DataTypes);
+db.RefreshToken = User(sequelize, Sequelize, DataTypes);
 
 export default db;
